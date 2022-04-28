88f7c36b5d63c6297094f9acec38896f8ede2646
Williker Carmo <williker.c.carmo@noesis.pt>
2022-04-28 15:59:50 +0100

    Add Shopping Cart and Application User Repository

---
+++ BulkyBook.DataAccess/Repository/ShoppingCartRepository.cs
@@ -0,0 +1,21 @@
+﻿using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.DataAccess.Repository
+{
+    public class ShoppingCartRepository : Repository<ShoppingCart>, IShoppingCartRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ShoppingCartRepository(ApplicationDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+    }
+}
--- BulkyBook.DataAccess/Repository/UnitOfWork.cs
+++ BulkyBook.DataAccess/Repository/UnitOfWork.cs
@@ -18,12 +18,18 @@ namespace BulkyBook.DataAccess.Repository
             CoverType = new CoverTypeRepository(_context);
             Product = new ProductRepository(_context);
             Company = new CompanyRepository(_context);
+            ApplicationUser = new ApplicationUserRepository(_context);
+            ShoppingCart = new ShoppingCartRepository(_context);
         }
         public ICategoryRepository Category { get; private set; }
 
         public ICoverTypeRepository CoverType { get; private set; }
         public IProductRepository Product { get; private set; }
         public ICompanyRepository Company { get; private set; }
+
+        public IApplicationUserRepository ApplicationUser { get; private set; }
+
+        public IShoppingCartRepository ShoppingCart { get; private set; }
 
         public void Save()
         {
