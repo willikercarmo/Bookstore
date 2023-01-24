3aa0b0347c18ecb6281c357fed2d022e52d1a106
Williker Carmo <carmo.williker@gmail.com>
2023-01-24 12:08:51 +0000

    Add dependency injection in Category Controller

--- BulkyBook/BulkyBook.Web/Controllers/CategoryController.cs
+++ BulkyBook/BulkyBook.Web/Controllers/CategoryController.cs
@@ -1,11 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BulkyBook.Web.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyBook.Web.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
+            var objCategoryList = _context.Categories.ToList();
             return View();
         }
     }
