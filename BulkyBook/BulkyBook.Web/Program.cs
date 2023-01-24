1d1a0986e9b995b43e92d2423560ad25056acc0a
Williker Carmo <carmo.williker@gmail.com>
2023-01-24 11:39:33 +0000

    Setup Program.cs to use DbContext

--- BulkyBook/BulkyBook.Web/Program.cs
+++ BulkyBook/BulkyBook.Web/Program.cs
@@ -1,8 +1,16 @@
+using BulkyBook.Web.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
+
+// Add DbContext and install Microsoft.EntityFrameworkCore.SqlServer
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("DefaultConnection")
+    ));
 
 var app = builder.Build();
 
