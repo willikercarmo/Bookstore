d0d5d7b07377f2b857a5a70035798d8f97560fd0
Williker Carmo <carmo.williker@gmail.com>
2023-01-24 14:22:25 +0000

    Edit Category - GET

--- BulkyBook/BulkyBook.Web/Controllers/CategoryController.cs
+++ BulkyBook/BulkyBook.Web/Controllers/CategoryController.cs
@@ -49,5 +49,52 @@ namespace BulkyBook.Web.Controllers
             // Redirect to Index action (Category list)
             return RedirectToAction("Index");
         }
+
+        // GET
+        public IActionResult Edit(int? id)
+        {
+            // Validate if id is null or zero
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            // Get category by id from database
+            var categoryFind = _context.Categories.Find(id);
+            //var categoryFirst = _context.Categories.FirstOrDefault(c => c.Id == id);
+            //var categorySingle = _context.Categories.SingleOrDefault(c => c.Id == id);
+
+            if (categoryFind == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFind);
+        }
+
+        // POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category category)
+        {
+            // Validate if Name field is equal to DisplayOrder field
+            if (category.Name == category.DisplayOrder.ToString())
+            {
+                // Set a custom error message
+                ModelState.AddModelError("CustomError", "The DisplayOrder cannot exactly match the Name.");
+            }
+
+            // Validate if the object is valid 
+            if (!ModelState.IsValid) return View(category);
+
+            // Add the new category
+            _context.Categories.Add(category);
+
+            // Save the new category to database
+            _context.SaveChanges();
+
+            // Redirect to Index action (Category list)
+            return RedirectToAction("Index");
+        }
     }
 }
