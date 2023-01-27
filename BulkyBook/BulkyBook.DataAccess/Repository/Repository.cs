3100985680fcc25c3dc4a149a0262b66273b87d1
Williker Carmo <carmo.williker@gmail.com>
2023-01-27 21:06:46 +0000

    Implement Repository Pattern

---
+++ BulkyBook/BulkyBook.DataAccess/Repository/Repository.cs
@@ -0,0 +1,46 @@
+﻿using BulkyBook.DataAccess.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace BulkyBook.DataAccess.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _context;
+        internal DbSet<T> _dbSet;
+        public Repository(ApplicationDbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+        public void Add(T entity)
+        {
+            _dbSet.Add(entity);
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            IQueryable<T> query = _dbSet;
+
+            return query.ToList();
+        }
+
+        public T? GetFirstOrDefault(Expression<Func<T, bool>> filter)
+        {
+            IQueryable<T> query = _dbSet;
+            query = query.Where(filter);
+
+            return query.FirstOrDefault();
+        }
+
+        public void Remove(T entity)
+        {
+            _dbSet.Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<T> entity)
+        {
+            _dbSet.RemoveRange(entity);
+        }
+    }
+}
