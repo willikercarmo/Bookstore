68f126ef8b0d8e117f0cbf113031c4c107a57359
Williker Carmo <carmo.williker@gmail.com>
2023-02-19 11:52:54 +0000

    Create Product Controller

---
+++ BulkyBook/BulkyBook.Web/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBook.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private readonly IUnitOfWork _context;
+
+        public ProductController(IUnitOfWork context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
